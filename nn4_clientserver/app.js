a2a72e6d2880efd9357f4c4b7ede2d96677f1fd7
Piyush Rathi <145590327+piyushr6@users.noreply.github.com>
2025-03-12 00:16:08 +0530

    mongo

--- nn4_clientserver/app.js
+++ nn4_clientserver/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs')
 
 //express app
 const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://piyushrathi06:FvpflzVrvYe69c5m@nodetut.jbqaa.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=nodetut'
+
+mongoose.connect(dbURI)
+   .then((result) => {
+      app.listen(3000);
+      console.log('connected to db');
+   })
+   .catch((err) => console.log(err));
 
 //register vire engine
 app.set('view engine', 'ejs');
 
 
 //listen for requests
-app.listen(3000);
+
 
 // app.use((req, res, next) => {
 //    console.log('new request made:');
@@ -19,7 +31,53 @@ app.listen(3000);
 //    next();
 // });
 //browser hangs here does not load page. we need next()
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
+
+app.get('/add-blog', (req, res) => {
+   const blog = new Blog({
+      title: 'new blog',
+      snippet: 'about my blog',
+      body: 'more about my new blog'
+   });
+
+   blog.save()
+      .then((result) => {
+         res.send(result)
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+});
+
+app.get('/all-blogs', (req, res) => {
+   Blog.find()
+      .then(result => {
+         res.send(result);
+      })
+      .catch(err => {
+         console.log(err);
+      });
+});
+
+app.get('/single-blog', (req, res) => {
+   Blog.findById('5ea99b49b8531f40c0fde689')
+      .then(result => {
+         res.send(result);
+      })
+      .catch(err => {
+         console.log(err);
+      });
+});
+
+app.get('/blogs', (req, res) => {
+   Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+         res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+         console.log(err);
+      });
+});
 
 
 //middleware and static files
