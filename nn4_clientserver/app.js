f2035d569c39137380e672807e76822d79e8338d
Piyush Rathi <145590327+piyushr6@users.noreply.github.com>
2025-03-11 09:48:06 +0530

    ejs

--- nn4_clientserver/app.js
+++ nn4_clientserver/app.js
@@ -3,27 +3,49 @@ const express = require('express');
 //express app
 const app = express();
 
+//register vire engine
+app.set('view engine', 'ejs');
+
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
    // res.send('<p>Homepage</p>');
-   res.sendFile('./views/index.html', { root: __dirname });
+   // res.sendFile('./views/index.html', { root: __dirname });
+
+   //blogs : blogs === blogs
+
+   const blogs = [
+      { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+      { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+      { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+   ];
+
+   res.render('index', { title: 'Home', blogs });
+
 });
 
 
 app.get('/about', (req, res) => {
    // res.send('<p>about</p>');
-   res.sendFile('./views/about.html', { root: __dirname });
+   // res.sendFile('./views/about.html', { root: __dirname });
+   res.render('about', { title: 'About' });
+});
+
+
+app.get('/blogs/create', (req, res) => {
+   res.render('create', { title: 'Home' });
 });
 
 
 //redirects
 app.get('/about-us', (req, res) => {
-   res.redirect('/about');
+   res.redirect('/about', { title: 'About' });
 });
 
 //404 page
 app.use((req, res) => {
-   res.status(404).sendFile('./views/404.html', { root: __dirname });
+   res.status(404).render('404', { title: '404' });
 });
+
